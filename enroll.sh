4fd96004e4331713bd78f7159fc61a68ebd1b4e1
Chillybeans <k.ingen.schenau@gmail.com>
2017-02-21 10:48:04 +0100

    Add files via upload

---
+++ enroll.sh
@@ -0,0 +1,39 @@
+!#/bin/sh
+
+#Create directory to mount temporary shares to
+mkdir /mnt/temp
+
+#disable firewalld service and stop it from starting at boot
+service firwalld stop
+chkconfig firewalld off
+
+#create directory to mount CDROM drive to
+#mkdir /mnt/cdrom
+#mount /dev/cdrom /mnt/cdrom
+
+#mount groups share
+mount -t cifs //g-star.raw/data/install -o username=_svc_compadd,domain=G-STAR.RAW,password=R@wadd00 /mnt/tmp
+#copy VMWare Tools Installation
+cd /mnt/tmp/Server/Linux
+mkdir /tmp
+cp VMwareTools-9.0.15-2323214.tar.gz /tmp
+
+#Un-tar and run
+cd /tmp
+tar -zxvf VMwareTools-9.0.15-2323214.tar.gz
+cd vmware-tools-distrib
+yum install perl gcc make kernel-headers kernel-devel -y
+rpm -ql kernel-headers
+sudo yum install "kernel-devel-uname-r == $(uname -r)"
+./vmware-install.pl -d default
+
+#Install packages needed for domain join
+yum -y install realmd sssd oddjob oddjob-mkhomedir adcli samba-common
+#Join Domain
+realm join --user=_svc_compadd --one-time-password=R@wadd00 G-STAR.RAW
+#Deny access to everyone
+realm deny --all
+#Allow the RDP group
+realm permit -g G-STAR.RAW\\rdp-$HOSTNAME
+#Give them sudo permission
+echo "%rdp-$HOSTNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
